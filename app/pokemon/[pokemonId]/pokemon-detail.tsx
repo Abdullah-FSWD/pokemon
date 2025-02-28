d5d9e2786618f46f8d70e17cfc0f3cb6409d29cf
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-02-28 12:46:56 +0530

    feat: stats field completed

--- app/pokemon/[pokemonId]/pokemon-detail.tsx
+++ app/pokemon/[pokemonId]/pokemon-detail.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { ArrowLeft, Heart, Share } from "lucide-react";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Progress } from "@/components/ui/progress";
 
 type pokemonDetailsProps = {
   data: any;
@@ -15,7 +16,7 @@ export function PokemonDetails({ data, description }: pokemonDetailsProps) {
     .split(" ");
   console.log("data", data);
   return (
-    <div className="container">
+    <div className="max-w-5xl m-auto">
       <div className="border-muted-foreground border p-5 opacity-50 rounded-sm flex justify-between hover:bg-muted hover:shadow">
         <ArrowLeft className="size-5" />
         <div className="flex justify-end gap-4">
@@ -23,8 +24,8 @@ export function PokemonDetails({ data, description }: pokemonDetailsProps) {
           <Share className="size-5" />
         </div>
       </div>
-      <div className="p-4 border-foreground border my-4 flex gap-2">
-        <div className="border border-muted-foreground rounded-full p-5 bg-slate-400">
+      <div className="p-4 border-foreground border my-4 md:flex block gap-2">
+        <div className="p-5">
           <Image
             src={data?.sprites.other.dream_world.front_default}
             alt="image"
@@ -85,22 +86,48 @@ export function PokemonDetails({ data, description }: pokemonDetailsProps) {
               </p>
             </div>
           </div>
-          <Tabs defaultValue="stats">
-            <TabsList className="flex w-full justify-between">
-              <TabsTrigger value="stats" className="w-full text-center">
-                Stats
-              </TabsTrigger>
-              <TabsTrigger value="moves" className="w-full text-center">
-                Moves
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="stats">
-              Make changes to your account here.
-            </TabsContent>
-            <TabsContent value="moves">Change your password here.</TabsContent>
-          </Tabs>
         </div>
       </div>
+      <Tabs className="p-5" defaultValue="stats">
+        <TabsList className="flex w-full justify-between">
+          <TabsTrigger value="stats" className="w-full text-center">
+            Stats
+          </TabsTrigger>
+          <TabsTrigger value="moves" className="w-full text-center">
+            Moves
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="stats" className="border-muted border">
+          {data.stats.map((stat, index) => (
+            <div key={index} className="p-1">
+              <p className="capitalize px-3 pt-2 bg-muted rounded-md ">
+                {"  "}
+                {stat.stat.name}
+              </p>
+              <div className="flex bg-muted px-2 py-1 items-center ">
+                <Progress
+                  className="flex-1 items-center"
+                  value={stat.base_stat}
+                />
+                <span className=" bg-muted text-center text-muted-foreground p-1">
+                  {stat.base_stat} / 90
+                </span>
+              </div>
+            </div>
+          ))}
+        </TabsContent>
+        <TabsContent value="moves">
+          {data.moves.map((move, index) => (
+            <div key={index} className="p-1">
+              <p className="capitalize px-3 pt-2 bg-muted rounded-md ">
+                {"  "}
+                {move.move.name}
+              </p>
+              <div className="flex bg-muted px-2 py-1 items-center"></div>
+            </div>
+          ))}
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
